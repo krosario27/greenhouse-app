c12a2c1487142d52a5cb3fb30e421203cffabfad
Kyle <kylewhite369@gmail.com>
2024-10-07 20:02:56 +1300

    Finished schema design

---
+++ models/Greenhouse.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+//Define schema for sensor readings
+const readingSchema = new mongoose.Schema({
+    timestamp: { type: Date, required: true },
+    temperature: { type: Number },
+    humidity: { type: Number },
+    soil_moisture: { type: Number },
+    co2_levels: { type: Number }
+});
+
+// Define schema for sensors (internal/external)
+const sensorSchema = new mongoose.Schema({
+    sesnor_id: { type: Number, required: true },
+    sensor_type: { type: String, enum: ['Internal', 'External'], required: true },
+    readings: [readingSchema]
+});
+
+// Define schema for greenhouses.
+const greenhouseSchema = new mongoose.Schema({
+    greenhouse_id: { type: String, required: true },
+    sensors: [sensorSchema]
+});
+
+// Define Location that houses multiple greenhouses
+const locationSchema = new mongoose.Schema({
+    locaiton: { type: String, required: true },
+    greenhouses: [greenhouseSchema]
+});
+
+// Create and export
+const Location = mongoose.model('Location', locationSchema);
+
+module.exports = Location
